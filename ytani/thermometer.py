01f09a2207da3e809265e3429ba4b07bcadd79cb
ytani01 <yoichi@tanibayashi.jp>
2018-03-20 06:19:49 +0900

    some fixes

--- ytani/thermometer.py
+++ ytani/thermometer.py
@@ -8,25 +8,39 @@ from BME280I2C import BME280I2C
 from AQM0802A import AQM0802A
 
 def main():
+    global lcd
+
     bme280ch = BME280I2C(0x76)
     lcd = AQM0802A()
     lcd.init_lcd()
 
+    t = ''
+    h = ''
     i = 0
     while bme280ch.meas():
-        print('{0:5.2f} C {1:5.2f} %'.format(bme280ch.T, bme280ch.H))
+        prev_t = t
+        prev_h = h
+        t = '{0:4.1f} C'.format(bme280ch.T)
+        h = '{0:4.1f} %'.format(bme280ch.H)
+
         lcd.clear()
-        lcd.print('{0:4.1f} C'.format(bme280ch.T))
+        lcd.print(t)
         if i == 0:
-            lcd.print(' .')
+            lcd.print('.')
 
         lcd.sec_line()
-        lcd.print('{0:4.1f} %'.format(bme280ch.H)) 
+        lcd.print(h)
         if i == 1:
-            lcd.print(' .')
+            lcd.print('.')
+
+        if t != prev_t:
+            print(t+', '+h)
 
         i = (i + 1) % 2
         sleep(1)
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    finally:
+        lcd.clear()
